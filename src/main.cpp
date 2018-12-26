bac4949bbf118a9fdf6648724cf2504db36720a7
Robert Lechner <lechnerrobert@gmx.at>
2018-12-26 19:49:11 +0100

    ESP now with "root" homepage improved TunableWhite

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,13 @@ uint32_t extractNumber(const char *data, int *start, bool *eof) {
   return result;
 }
 
+uint8_t plus10(uint16_t value) {
+  return max(0, (value-10)*100/90);
+}
+
+uint8_t minus10(uint16_t value) {
+  return min(100, value*100/90);
+}
 
 void networkData(char *data, int datalen){
    int p1 = 0;
@@ -76,13 +83,14 @@ void networkData(char *data, int datalen){
        isOnOff = queue.add(startChannel, updSp, newValue, gamma, true);
        queue.update(startChannel, onoffSpeed, isOnOff);
      } else if  (data[0 + 3] == 'W') {
-       isOnOff = queue.add(startChannel, updSp, newValue, 3, true);
-       isOnOff = queue.add(startChannel + 1, updSp, newValue, 2, true) | isOnOff;
+       
+       isOnOff = queue.add(startChannel, updSp, plus10(newValue), 3, true);
+       isOnOff = queue.add(startChannel + 1, updSp, minus10(newValue), 2, true) | isOnOff;
        queue.update(startChannel, onoffSpeed, isOnOff);
        queue.update(startChannel + 1, onoffSpeed, isOnOff);
      } else if  (data[0 + 3] == 'V') {
-       isOnOff = queue.add(startChannel, updSp, newValue, 2, true);
-       isOnOff = queue.add(startChannel + 1, updSp, newValue, 3, true) | isOnOff;       
+       isOnOff = queue.add(startChannel, updSp, minus10(newValue), 2, true);
+       isOnOff = queue.add(startChannel + 1, updSp, plus10(newValue), 3, true) | isOnOff;       
        queue.update(startChannel, onoffSpeed, isOnOff);
        queue.update(startChannel + 1, onoffSpeed, isOnOff);
      } else if  (data[0 + 3] == 'S') {
--- src/otaupdate_ESP.cpp
+++ src/otaupdate_ESP.cpp
@@ -1,5 +1,6 @@
 #if USE_OTAUPDATE==1
 
+
 #include "otaupdate.h"
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPUpdateServer.h>
@@ -8,6 +9,19 @@ ESP8266WebServer httpServer(81);
 ESP8266HTTPUpdateServer httpUpdater;
 
 
+void AddRootPage() {
+  httpServer.on("/", HTTP_GET, [&](){
+      httpServer.client().setNoDelay(true);
+      const char compile_date[] = __DATE__ " " __TIME__;
+      String tmp;
+      tmp = String("<p>UDPtoDMX H801 Version ") + String(compile_date) + 
+        "<br /><br /><a href=\"/update\">OTA-Update</a><br /><br /><a href=\"https://github.com/LechnerRobert/UDPtoDMX\">Project Homepage</a></p>";
+      httpServer.send(200, F("text/html"), tmp);
+      delay(100);
+      httpServer.client().stop();
+  });    
+}
+
 
 void otaUpdateClass::loop() { 
   httpServer.handleClient();
@@ -15,7 +29,9 @@ void otaUpdateClass::loop() {
 
 void otaUpdateClass::init() { 
   httpUpdater.setup(&httpServer);
+  AddRootPage();
   httpServer.begin();  
+
 }
 
 otaUpdateClass otaUpdate;
